9f97eca88b1197094ab171082d34970c5321ca86
muhammederkam85 <44413751+muhammederkam85@users.noreply.github.com>
2019-05-07 19:33:45 +0300

    Yürüme
    Karakterin sağa ve sola hareketleri için yazılan kod

---
+++ CharacterAndroid.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAndroid : MonoBehaviour
+{
+    Character kr;
+    void Start()
+    {
+        kr = GetComponent<Character>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+
+    public void sol()
+    {
+        kr.sol = true;
+    }
+
+
+    public void sag()
+    {
+        kr.sag = true;
+    }
+
+    public void SolUp()
+    {
+        kr.sol = false;
+    }
+
+    public void SagUp()
+    {
+        kr.sag = false;
+    }
+}
