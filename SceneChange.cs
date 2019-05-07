27fb7966cee7799dfb86ab5bc30e0b5b85f8671e
muhammederkam85 <44413751+muhammederkam85@users.noreply.github.com>
2019-05-07 19:41:36 +0300

    Sahne değişimi
    Sahne değiştirmek için yazılan kod

---
+++ SceneChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+
+    public void Leveller(int levelid)
+    {
+        SceneManager.LoadScene(levelid);
+    }
+
+    public void TekrarOyna()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
