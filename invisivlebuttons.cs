1f4cd181a1deace93ae6b7709846bfe7c1e4c380
muhammederkam85 <44413751+muhammederkam85@users.noreply.github.com>
2019-05-07 19:35:18 +0300

    Buton
    Butonları görünmez hale getirmek için yazılan kod

---
+++ invisivlebuttons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class invisivlebuttons : Text
+{
+
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+    
+}
