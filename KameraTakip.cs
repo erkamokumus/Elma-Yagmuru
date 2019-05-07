73b9b9dcc0a27cb3c01391df4d15340570a6dd6e
muhammederkam85 <44413751+muhammederkam85@users.noreply.github.com>
2019-05-07 19:35:59 +0300

    Kamera Takip
    Oyun kamerasının karakterimizi takip etmesi için yazılan kod

---
+++ KameraTakip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraTakip : MonoBehaviour
+{
+    public Transform karakter;
+    public float x, y;
+
+    void Start()
+    {
+        karakter = GameObject.FindGameObjectWithTag("character").transform;
+    }
+
+    
+    void Update()
+    {
+        transform.position = new Vector3(karakter.position.x + x , karakter.position.y + y , -10 );
+    }
+}
